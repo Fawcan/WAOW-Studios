68796296c73a3fe079378b45b5dc4a8f9d385e2e
Maria Karlsson <karlma@utb.fuv.se>
2016-02-24 11:31:47 +0100

    Lade till test mariaTest

---
+++ Enigma/Assets/myScripts/Player/mariaTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mariaTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        string hej = "hej";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
