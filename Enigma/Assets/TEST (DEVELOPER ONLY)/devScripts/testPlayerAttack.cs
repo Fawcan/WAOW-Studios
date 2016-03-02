dc021ecf7dbdc39a1f5d8b8361680c2ce1762bd1
Glänn <hallda@3se-spel-17.utb.fuv.se>
2016-03-02 15:26:19 +0100

    Changed input values for attack and position. Created a basic attack state for character and enemy tag system

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/testPlayerAttack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class testPlayerAttack : MonoBehaviour
+{
+       
+	
+	void Start ()
+    {
+        
+	}
+
+    
+	void Update ()
+    {
+	
+	}
+}
