362451b5ce0d260f70ee8aeeab32c09c6813b0e7
Albin Frisk <albinfrisk@gmail.com>
2016-03-02 22:43:20 +0100

    Character tweaks

--- Enigma/Assets/myScripts/Player/playerMovement.cs
+++ Enigma/Assets/myScripts/Player/playerMovement.cs
@@ -16,9 +16,9 @@ This is the final movement code
 
 public class playerMovement : MonoBehaviour
 {
-    public float mSpeed;
+
     public CharacterController controller;
-    private Vector3 position;
+    //private Vector3 position;
 
     // Run and idle animation for the player character.
     public AnimationClip run;
@@ -35,56 +35,24 @@ public class playerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime);
+            transform.Translate(Vector3.left * Time.deltaTime * 2);
             GetComponent<Animation>().Play(run.name);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime);
+            transform.Translate(Vector3.forward * Time.deltaTime * 2);
             GetComponent<Animation>().Play(run.name);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * Time.deltaTime);
+            transform.Translate(Vector3.back * Time.deltaTime * 2);
             GetComponent<Animation>().Play(run.name);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime);
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
             GetComponent<Animation>().Play(run.name);
         }
+
     } // End of Void Update()
-
-
-    void LocatePosition()
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit, 1000))
-        {
-            if (Vector3.Distance(gameObject.transform.position, hit.transform.position) < 5.0f) ;
-            //hit.transform.GetComponent<Enemy>().Damage(5.0f);
-        }
-        else if (hit.transform.tag == "Door")
-        {
-            //hit.transform.GetComponent<DoorScript>().OpenDoor();
-        }
-        else
-        {
-            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-        }
-    }// End LocatePosition function
-
-    void LooksAtMouse()
-    {
-        Quaternion newRotation = Quaternion.LookRotation(position - transform.position, Vector3.forward);
-
-        // Locks the x and z axis so you only turn
-        newRotation.x = 0f;
-        newRotation.z = 0f;
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
-        controller.SimpleMove(transform.forward * mSpeed);
-    }
 }
