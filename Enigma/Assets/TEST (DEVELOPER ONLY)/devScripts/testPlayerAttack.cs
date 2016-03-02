1483961b887ee1fd74d327f84ef1245e20aa8698
Glänn <hallda@3se-spel-17.utb.fuv.se>
2016-03-02 14:11:33 +0100

    Added a test script for development considering attack state. Also updated unity engine version to latest 5.3.3f1

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/testPlayerAttack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+    *** WAOW STUDIOS *** - David Halldin
+    Script is controlling wether the player enters a attackstate or not. The attackstate is to be aborted if the player moves
+    or dies.
+*/
+
+public class testPlayerAttack : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
