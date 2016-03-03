9559bbc48eedab91cf1663dd9950a15b814e89c8
Albin Frisk Pettersson <frisal@utb.fuv.se>
2016-03-03 14:44:22 +0100

    no message

--- Enigma/Assets/myScripts/Player/playerMovementComplete.cs
+++ Enigma/Assets/myScripts/Player/playerMovementComplete.cs
@@ -14,15 +14,17 @@ You need to name the terrain Floor and put it in a Floor layer
 public class playerMovementComplete : MonoBehaviour
 {
 
-    public CharacterController controller;
-    //private Vector3 position;
+    [SerializeField]
+    CharacterController controller; // The Players Character Controller
     Rigidbody playerRigidbody;  // Reference to the player's rigidbody.
     int floorMask;              // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
     float camRayLength = 100f;  // The length of the ray from the camera into the scene.
 
     // Run and idle animation for the player character.
-    [SerializeField] AnimationClip run;
-    [SerializeField] AnimationClip idle;
+    [SerializeField]
+    AnimationClip run;
+    [SerializeField]
+    AnimationClip idle;
 
     void Awake()
     {
