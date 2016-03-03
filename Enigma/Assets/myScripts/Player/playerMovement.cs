1df4fef27d9237c9dc8d3898aeb8aba2adb39e89
Albin Frisk Pettersson <frisal@utb.fuv.se>
2016-03-03 14:38:57 +0100

    Merged the movement and rotation scripts.

--- Enigma/Assets/myScripts/Player/playerMovement.cs
+++ Enigma/Assets/myScripts/Player/playerMovement.cs
@@ -31,25 +31,37 @@ public class playerMovement : MonoBehaviour
         // Idle animation
         GetComponent<Animation>().CrossFade(idle.name);
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            GetComponent<Animation>().Play(run.name);
+        }
+        /*if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+            GetComponent<Animation>().Play(run.name);
+        }*/
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * 2);
+            transform.Translate(Vector3.left * Time.deltaTime);
             GetComponent<Animation>().Play(run.name);
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * 2);
-            GetComponent<Animation>().Play(run.name);
-        }
+
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * 2);
+            transform.Translate(Vector3.back * Time.deltaTime);
             GetComponent<Animation>().Play(run.name);
         }
+        /*if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+            GetComponent<Animation>().Play(run.name);
+        }*/
+
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * 2);
+            transform.Translate(Vector3.right * Time.deltaTime);
             GetComponent<Animation>().Play(run.name);
         }
 
