fc00d8cfb300935a2749b23393338d51c5019224
unknown <bergan@3se-spel-02.utb.fuv.se>
2016-03-03 12:59:48 +0100

    no message

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/DestroyBarrel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Put the script on the camera. The destroyable object need a collider
+
+public class DestroyBarrel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) // When you click Left mouse button
+        {
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition); // Targets the object you click on
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Destroy(hit.transform.gameObject); // Destroy object
+            }
+        }
+    }
+
+}
