5be68fb0987f5752a33761e0f554038011f110bf
unknown <bergan@3se-spel-02.utb.fuv.se>
2016-03-07 15:11:10 +0100

    no message

---
+++ Enigma/Assets/myScripts/Objects/Barrel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    public int hp = 1;
+
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	
+	}
+	
+    public void DamageBarrel (int loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <=0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
