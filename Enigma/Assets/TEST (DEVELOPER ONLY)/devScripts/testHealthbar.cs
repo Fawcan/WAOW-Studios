244d9a7d37ae9a343f39d63f8a42a8b71ff37949
Maria Karlsson <karlssonmaria1984@gmail.com>
2016-03-07 22:24:47 +0100

    Added a Healthbar, it doesn't do anything yet, except for looking pretty.

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/testHealthbar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testHealthbar : MonoBehaviour {
+
+    /*
+    Script created by Maria.
+
+    This script is used for the UI Healthbar.
+
+    */
+
+    
+    
+
+        // Use this for initialization
+        void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+}
