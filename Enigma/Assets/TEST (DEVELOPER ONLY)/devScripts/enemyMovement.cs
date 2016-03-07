5e3a152f2ea5506f49f90f0886b2161f2e55750b
Albin Frisk Pettersson <frisal@utb.fuv.se>
2016-03-07 15:21:07 +0100

    Made changes in my scene

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/enemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMovement : MonoBehaviour
+{
+    Rigidbody enemyRigidBody;
+    public Transform destination;
+    private NavMeshAgent agent;
+    [SerializeField]
+    Transform mPlayer;
+    float mTurnSpeed = 100f;
+
+    // Run and idle animation for the enemy character.
+    [SerializeField]
+    AnimationClip run;
+    [SerializeField]
+    AnimationClip idle;
+
+	
+	void Update()
+    {
+        GetComponent<Animation>().Play(run.name);                           // Running animation
+
+
+        Vector3 targetDir = mPlayer.position - transform.position;
+        float step = mTurnSpeed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDir);
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(destination.position);
+    }
+
+}
