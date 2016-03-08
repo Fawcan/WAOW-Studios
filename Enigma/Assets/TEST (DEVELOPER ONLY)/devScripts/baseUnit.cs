6f8f3bafe90ac3ca82c2743254e9a4cec9818516
Glänn <davidhalldin92@gmail.com>
2016-03-08 11:58:00 +0100

    Added a testscene by the name GlännScene and built a baseUnit.cs script for handler and managment. Script's placed within devScripts folder Initialized core script structure

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/baseUnit.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+    *** DISCLAIMER ***
+    This script handels all base information and values for characters and are to be placed in the world as a GameObject by the name UNIT
+    Script to be handeld by given resource until otherwise
+*/
+public class baseUnit : MonoBehaviour
+{
+    int mHealth;
+    float mMovementSpeed;
+    float mAttackSpeed;
+    float mAttackRange;
+    int mEnergy;
+    int mDamage;
+    
+
+    void Start()
+    {
+        //write init variables here
+    }
+
+    void Update()
+    {
+       //do not write code here
+    }
+
+    public virtual void Move(Vector2 direction)
+    {
+        //input here
+    }
+
+    public virtual void Rotate(float angle)
+    {
+
+    }
+
+    public virtual void Attack()
+    {
+        //Attack input and events here
+    }
+
+}
