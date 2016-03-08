759a59011a4f30cdb5d2bc7010b0a8569a7ae0bf
Maria Karlsson <karlssonmaria1984@gmail.com>
2016-03-08 15:05:36 +0100

    Working healthbar, is currently set to automaticly decrease to show that it actually works.

---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/BillBoard.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillBoard : MonoBehaviour {
+
+    /*
+    Script by Maria
+
+        This Scipt is used for controlling the enemy healthbars
+        to make sure that they will always face the mein camera 
+        and not rotate when the enemy rotates. Please add this
+        script as a component on the enemy-canvas and drag the 
+        main camera to the mCamera slot.
+
+    */
+
+    public Camera mCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(transform.position + mCamera.transform.rotation * Vector3.back, mCamera.transform.rotation * Vector3.down); //If the healthbar is backwards try changing 'back' to 'front' or 'down' to 'up' on Vector3
+	
+	}
+}
---
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/userInterface.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class userInterface : MonoBehaviour {
+
+    /*
+    Script by Maria
+
+        This script should be used for ALL User interface
+        that is connected to the player, for example
+        'Healthbar', 'Perk-cooldown' and 'number of potions 
+        in stock'.
+        Make sure to add game object 'Healthfull' to the 'M
+        Health Bar' field in the inspector after adding this
+        script to Canvas components.
+        
+    */
+
+    [SerializeField]
+    private GameObject mHealthBar;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    //This function is called by the 'testHealthBar' Script.
+    public void SetHealthBar(float mHealth)
+    {
+        mHealthBar.transform.localScale = new Vector3(Mathf.Clamp(mHealth, 0f, 1f), mHealthBar.transform.localScale.y, mHealthBar.transform.localScale.z);
+    }
+}
