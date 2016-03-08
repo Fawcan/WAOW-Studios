668ce442bfa772e9fe41a9c6ce8eb7a89eb184c0
Maria Karlsson <karlssonmaria1984@gmail.com>
2016-03-08 10:33:23 +0100

    no message

---
+++ Enigma/Assets/myScripts/ChestOpen.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestOpen : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private bool mChestOpen = false;
+    private Animation mAnimChest;
+    private float mInteractDist = 20f;
+
+    void Start()
+    {
+        mAnimChest = GetComponent<Animation>();
+        mAnimChest["ChestAnim"].wrapMode = WrapMode.ClampForever;
+    }
+
+    void Update()
+    {
+        //Checks if player has clicked and if player is within collision, then play chest animation.
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, mInteractDist))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    gameObject.GetComponent<Collider>().enabled = false;
+                    GetComponent<Animation>().Play("ChestAnim");
+                }
+            }
+        }
+    }
+}
