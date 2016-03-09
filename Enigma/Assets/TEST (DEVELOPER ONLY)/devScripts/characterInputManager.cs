77427991f46ac9fa6f5dbe8e9ec049634cef5930
Maria Karlsson <karlma@utb.fuv.se>
2016-03-09 12:41:21 +0100

    Added disclaimer and structure to code

--- Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/characterInputManager.cs
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/characterInputManager.cs
@@ -1,19 +1,32 @@
-﻿/*using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-// ******************* NEED TO LOOK AT THIS SCRIPT
+/*
+    Script by Maria and David
+    [DISCLAIMER] ***
+    
+    ***SCRIPT IS UNDER DEVELOPMENT DO NOT OPEN OR WRITE CODE UNTIL OTHERWISE***
+    - Add descritption and disclaimer for this script [X]
+    - Script contains defined input systems and what the actions provide. []
+    - Script is checked and uses at least one 'Debug.Log' statement []
+    - Input in script is correspondent to its update function. []
+
+    characterInputManager controls the given input and returns value to players movement represented.
+    This script is independent and not to be inherited unless needed.
+*/
+
+public class characterInputManager : MonoBehaviour
+{
 
 
-public class characterInputManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+    {
 	
 	}
 }
-*/
