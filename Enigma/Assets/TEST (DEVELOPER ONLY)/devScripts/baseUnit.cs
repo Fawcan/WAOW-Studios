36a5bed763f1f30ff344d53adfa54bd3265d7484
Albin Frisk Pettersson <frisal@utb.fuv.se>
2016-03-09 15:06:38 +0100

    So I can start it

--- Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/baseUnit.cs
+++ Enigma/Assets/TEST (DEVELOPER ONLY)/devScripts/baseUnit.cs
@@ -1,43 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 /*
+
+    // !!! DO NOT OPEN THIS SCRIPT !!!
+
     *** DISCLAIMER ***
     This script handels all base information and values for characters and are to be placed in the world as a GameObject by the name UNIT
     Script to be handeld by given resource until otherwise
 */
 public class baseUnit : MonoBehaviour
 {
+    [SerializeField]
     int mHealth;
-    float mMovementSpeed;
+    [SerializeField]
+    float mSpeed;
+    [SerializeField]
     float mAttackSpeed;
+    [SerializeField]
     float mAttackRange;
-    int mEnergy;
+    [SerializeField]
     int mDamage;
-    
+    // [SerializeField]
+    // int mEnergy;
 
     void Start()
     {
-        //write init variables here
+        mHealth = 10;
+        mSpeed = 2;
+        mAttackSpeed = 1;
+        mAttackRange = 10;
+        mDamage = 3;
+
     }
 
     void Update()
     {
-       //do not write code here
-    }
 
-    public virtual void Move(Vector2 direction)
-    {
-        //input here
-    }
-
-    public virtual void Rotate(float angle)
-    {
 
     }
 
-    public virtual void Attack()
-    {
-        //Attack input and events here
-    }
+    //virtual void Move(Vector2 direction)
+    //{
+
+
+    //}
+
+    //virtual Rotate()
+    //{
+
+
+    //}
+
+    //virtual Attack()
+    //{
+
+
+    //}
 
 }
