244948d60e2fc7532c49d67ad9deca8f1db066c3
Glänn <hallda@3se-spel-17.utb.fuv.se>
2016-03-10 10:42:34 +0100

    Added a enemy script that ""ärvs" from  base unit

---
+++ Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : BaseUnit
+{
+    [SerializeField]
+    protected int mEnemyHP = 10;
+    [SerializeField]
+    protected float mEnemySpeed = 2f;
+    [SerializeField]
+    protected float mEnemyAttackSpeed = 1f;
+    [SerializeField]
+    protected float mEnemyAttackRange = 10f;
+    [SerializeField]
+    protected int mEnemyDamage = 2;
+
+    void Awake()
+    {
+        GetComponent<Animation>();
+        GetComponent<Rigidbody>();
+    }
+
+    public override void Move(Vector2 direction)
+    {
+        base.Move(direction);
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+
+    }
+
+    public override void Rotate(Quaternion rotation)
+    {
+        base.Rotate(rotation);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
+
+}
