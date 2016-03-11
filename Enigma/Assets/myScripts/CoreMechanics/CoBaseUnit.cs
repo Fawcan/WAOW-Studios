91f3266291f0213fb9925d9769cadd5dddd1ed31
Glänn <hallda@3se-spel-17.utb.fuv.se>
2016-03-11 15:14:46 +0100

    Updated values in CoBaseUnit.cs

--- Enigma/Assets/myScripts/CoreMechanics/CoBaseUnit.cs
+++ Enigma/Assets/myScripts/CoreMechanics/CoBaseUnit.cs
@@ -3,6 +3,16 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/*
+    ** WAOW STUDIOS ** - David Halldin
+    CoBaseUnit.cs 
+    [DISCLAIMER]
+    Script is under development and to be handeld only by given resource until otherwise given OKAY!
+    This script handels all base information and values for characters and are to be placed in the world as a GameObject by the name UNIT***
+    Sript
+*/
+
+
 [RequireComponent(typeof(Animation))]
 [RequireComponent(typeof(Rigidbody))]
 
@@ -18,10 +28,40 @@ public class CoBaseUnit : MonoBehaviour
     //Protected variables below
     protected Animator mAnimatorPlayer;
     protected Rigidbody mRigidBody;
+
     //Private variables below
     private float mAttackSpeedCounter;
 
-	//Serialized private variables below
-    //Public variables below
-    //Component constructors below
+    void Awake()
+    {
+        //Fetch Nessecary components such as animation rigs and rigidbody components
+        //Initialize them in correct order and use void OnEnable() if nessecary.
+        mRigidBody = GetComponent<Rigidbody>();
+    }//End void Awake
+
+    protected void PlayAnimation(string mAnimationName)
+    {
+        switch(mAnimationName)
+        {
+            //Changes states of animation
+        }
+
+    }//End Function PlayAnimation()
+
+    public virtual void Rotate(Quaternion rotation)
+    {
+        mRigidBody.MoveRotation(rotation);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public virtual void ApplyDamage(int damage)
+    {
+
+    }
+
+
 }
