80743b2d2b2bf5a08acdca1cca3dc87931e470af
Glänn <hallda@3se-spel-17.utb.fuv.se>
2016-03-11 15:01:00 +0100

    Added Core folders and new CoreBasUnit script. Variables init only

---
+++ Enigma/Assets/myScripts/CoreMechanics/CoBaseUnit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animation))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class CoBaseUnit : MonoBehaviour
+{
+    //Serialized protected variables below
+    [SerializeField] protected int mHealth;
+    [SerializeField] protected float mSpeed;
+    [SerializeField] protected float mAttackSpeed;
+    [SerializeField] protected float mAttackRange;
+    [SerializeField] protected int mDamage;
+
+    //Protected variables below
+    protected Animator mAnimatorPlayer;
+    protected Rigidbody mRigidBody;
+    //Private variables below
+    private float mAttackSpeedCounter;
+
+	//Serialized private variables below
+    //Public variables below
+    //Component constructors below
+}
