de74dabf989395cf62a7d907797af721386ae50e
Maria Karlsson <karlssonmaria1984@gmail.com>
2016-03-14 16:26:36 +0100

    Player and Enemy basic animation is set up in the animation-controllers. Included description and How To's in EnemyAnimator.cs and PlayerAnimator.cs

--- Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/EnemyAnimator.cs
+++ Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/EnemyAnimator.cs
@@ -2,23 +2,46 @@
 using System.Collections;
 
 public class EnemyAnimator : MonoBehaviour {
+    
+    /*
+    Script by Maria
 
-    private Animator mAnimator;
+    *** READ THIS FIRST ***
 
-    // Use this for initialization
-    void Start ()
-    {
+    Add the Animator controller in the inspector of the skeleton
+    and make sure that 'EnemyAnimator' is in the controller-
+    field and the 'skeletonAvatar' is in the avatar-field.
+    Place the following lines below under the Enemy's
+    corresponding states in the Enemy script.
 
-        mAnimator = GetComponent<Animator>();
+    Note that 'mAnimatorPlayer' will be used for the PLAYER 
+    animations and 'mAnimatorEnemy' will be used for ENEMY 
+    animations!
 
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
+    (Animation parameter -setTrigger automaticly resets the animation 
+    to false when the animation have finished, unlike the -SetBool
+    parameter which you need to reset manually through code.)
+    */
 
-        // Animation parameter for blend between Walking and Idle
-        mAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+    // Add this before void Start()
+    //      protected Animator mAnimatorEnemy;
 
-    }
+
+    // In void Start() add this
+    //      mAnimatorEnemy = GetComponent<Animator>();
+
+
+    // Animation parameter for blend between WALKING and IDLE
+    //      mAnimatorEnemy.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+
+
+    // Triggers ATTACK animation
+    //      mAnimatorEnemy.SetTrigger("Attacking");
+
+
+    // Bool for DEATH animation
+    //      mAnimatorPlayer.SetBool("Die", true);
+    // Before player respawn set bool to false:
+    //      mAnimatorPlayer.SetBool("Die", false);
+
 }
--- Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/PlayerAnimator.cs
+++ Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/PlayerAnimator.cs
@@ -3,21 +3,49 @@ using System.Collections;
 
 public class PlayerAnimator : MonoBehaviour {
 
-    private Animator mAnimator;
+    /*
+    Script by Maria
 
-	// Use this for initialization
-	void Start ()
-    {
+        *** READ THIS FIRST ***
 
-        mAnimator = GetComponent<Animator>();
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        // Animation parameter for blend between Walking and Idle
-        mAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
-	
-	}
+    Add the Animator controller in the inspector of the player
+    and make sure that 'PlayerAnimator' is in the controller-
+    field and the 'Ganfaul_M_AureAvatar' is in the avatar-field.
+    Place the following lines below under the player's
+    corresponding states in the Player script.
+
+    Note that 'mAnimatorPlayer' will be used for the PLAYER 
+    animations and 'mAnimatorEnemy' will be used for ENEMY 
+    animations!
+
+    (Animation parameter -setTrigger automaticly resets the animation 
+    to false when the animation have finished, unlike the -SetBool
+    parameter which you need to reset manually through code.)
+    */
+
+
+    // Add this before void Start()
+    //      protected Animator mAnimatorPlayer;
+
+    // In void Start() add this
+    //      mAnimatorPlayer = GetComponent<Animator>();
+
+
+    // Animation parameter for blend between WALKING and IDLE
+    //      mAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+
+    // Triggers ATTACK animation
+    //      mAnimatorPlayer.SetTrigger("Attacking");
+
+
+    // Bool for DEATH animation
+    //      mAnimatorPlayer.SetBool("Die", true);
+    // Before player respawn set bool to false:
+    //      mAnimatorPlayer.SetBool("Die", false);
+
+
+    // Triggers TAKE DAMAGE animation
+    //      mAnimatorPlayer.SetTrigger("Hit");
+
+
 }
