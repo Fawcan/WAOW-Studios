c2c9e80a0a0366eb41bb0a3f411a38cc08d48f88
Maria Karlsson <karlma@utb.fuv.se>
2016-03-17 16:35:04 +0100

    All enemy animations set back to Legacy.

--- Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/Enemy.cs
+++ Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/Enemy.cs
@@ -10,21 +10,10 @@ using System.Collections;
 public class Enemy : BaseUnit
 {
     private NavMeshAgent agent;
-    private Animator mAnimatorEnemy;
-
-    void Start()
-    {
-        mAnimatorEnemy = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        mAnimatorEnemy.SetFloat("EVSpeed", Input.GetAxis("Vertical"));
-    }
-
-
+    
     public void Move(Vector3 destination)
     {
+        base.PlayAnimation("run");
         agent = gameObject.GetComponent<NavMeshAgent>();
         agent.SetDestination(destination);
     }
