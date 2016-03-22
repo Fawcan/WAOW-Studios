afc0edbbd29acca13e683f86b03802fd8e90945d
unknown <bergan@3se-spel-02.utb.fuv.se>
2016-03-22 15:55:20 +0100

    Light script and object added to Chest, Door and Barrel Prefabs

---
+++ Enigma/Assets/TEST (DEVELOPMENT ONLY)/devScripts/lightswitch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightswitch : MonoBehaviour
+{
+    [SerializeField]
+    private Light objLight;
+
+    // check if somthing enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        // if its the Player
+        if (other.gameObject.tag == "Player")
+        {
+            // turn on the light
+            objLight.enabled = true;
+        }
+    }
+    // check if somthing leaves the trigger
+    void OnTriggerExit(Collider other)
+    {
+        // if its the Player
+        if (other.gameObject.tag == "Player")
+        {
+            // turn off the light
+            objLight.enabled = false;
+        }
+    }
+
+
+}
